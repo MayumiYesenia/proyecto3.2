94fd20580e4525b405fe5ba5a086a6824cd9688e
Leonel Campuzano <leonel.campuzano@utec.edu.pe>
2023-11-20 17:19:55 -0500

    react

--- mobile/App.js
+++ mobile/App.js
@@ -1,27 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { REACT_APP_BACKEND_URL } from '@env';
-
-axios.get(REACT_APP_BACKEND_URL)
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+import React from 'react';
+import HomeScreen from './home';
 
 
-
-
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  export default function App() {
+    return (
+      <HomeScreen />
+    );
+  }
 
 const styles = StyleSheet.create({
   container: {
